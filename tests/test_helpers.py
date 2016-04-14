995481d8f4133c841fc943605cab882aeef0d78f
elboyran <e.ranguelova@esciencecenter.nl>
2016-04-14 16:51:11 +0200

    binary_mask2ellipse_features now otputs also standard parameters; tests for polynomial parameters are updated

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -33,7 +33,7 @@ class HelpersEllipseTester(unittest.TestCase):
                 os.path.join(
                     testdata_path,
                     'Binary_ellipse1.png'), cv2.IMREAD_GRAYSCALE))
-        self.features_ellipse1 = 100.00 * \
+        self.features_poly_ellipse1 = 100.00 * \
             np.array([2.000000000000000, 1.750000000000000, 0.000008650519031, -0.000000000000000, 0.000051020408163, 0.020000000000000])
 
         self.ellipse2_mask = np.array(
@@ -41,7 +41,7 @@ class HelpersEllipseTester(unittest.TestCase):
                 os.path.join(
                     testdata_path,
                     'Binary_ellipse2.png'), cv2.IMREAD_GRAYSCALE))
-        self.features_ellipse2 = 100.00 * np.array([1.870000000000000,
+        self.features_poly_ellipse2 = 100.00 * np.array([1.870000000000000,
                                                     0.385000000000000,
                                                     0.000400000000000,
                                                     0.000000000000000,
@@ -53,7 +53,7 @@ class HelpersEllipseTester(unittest.TestCase):
                 os.path.join(
                     testdata_path,
                     'Binary_ellipse3.png'), cv2.IMREAD_GRAYSCALE))
-        self.features_ellipse3 = 100.00 * \
+        self.features_poly_ellipse3 = 100.00 * \
             np.array([1.019717800289436, 0.904095513748191, 0.000017518811785, -0.000000901804067, 0.000022518036288, 0.020000000000000])
 
         self.ellipse4_mask = np.array(
@@ -61,7 +61,7 @@ class HelpersEllipseTester(unittest.TestCase):
                 os.path.join(
                     testdata_path,
                     'Binary_ellipse4.png'), cv2.IMREAD_GRAYSCALE))
-        self.features_ellipse4 = 100.00 * np.array([0.653333333333333,
+        self.features_poly_ellipse4 = 100.00 * np.array([0.653333333333333,
                                                     1.860687093779016,
                                                     0.000040675758984,
                                                     0.000022724787475,
@@ -78,10 +78,10 @@ class HelpersEllipseTester(unittest.TestCase):
         coeff = [A, B, C]
         assert sr.helpers.array_diff(self.coeff, coeff)
 
-    def test_mask2features_ellipse1(self):
-       # print "---Testing binary_mask2ellipse_features for
+    def test_mask2features_poly_ellipse1(self):
+       # print "---Testing binary_mask2ellipse_features_single for
        # ellipse1----------------"
-        num_regions, features = sr.helpers.binary_mask2ellipse_features_single(
+        _, _, features = sr.helpers.binary_mask2ellipse_features_single(
             self.ellipse1_mask, self.connectivty, 2)
 
 #        print "MATLAB features:", self.features_ellipse1
@@ -90,16 +90,12 @@ class HelpersEllipseTester(unittest.TestCase):
 #        print 'Max abs. difference: ',  np.max(np.max( np.abs(features - self.features_ellipse1)))
 # print
 # '********************************************************************************************'
-        assert sr.helpers.array_diff(
-            self.features_ellipse1,
-            features,
-            self.rtol,
-            self.atol)
+        assert sr.helpers.array_diff(self.features_poly_ellipse1, features, self.rtol, self.atol)
 
-    def test_mask2features_ellipse2(self):
-        # print "---Testing binary_mask2ellipse_features for
+    def test_mask2features_poly_ellipse2(self):
+        # print "---Testing binary_mask2ellipse_features_single for
         # ellipse2----------------"
-        num_regions, features = sr.helpers.binary_mask2ellipse_features_single(
+        _, _, features = sr.helpers.binary_mask2ellipse_features_single(
             self.ellipse2_mask, self.connectivty, 2)
 
         # print "MATLAB features:", self.features_ellipse2
@@ -108,16 +104,12 @@ class HelpersEllipseTester(unittest.TestCase):
         # print 'Max abs.difference: ',  np.max(np.max( np.abs(features - self.features_ellipse2)))
         # print
         # '********************************************************************************************'
-        assert sr.helpers.array_diff(
-            self.features_ellipse2,
-            features,
-            self.rtol,
-            self.atol)
+        assert sr.helpers.array_diff(self.features_poly_ellipse2,  features,  self.rtol, self.atol)
 
-    def test_mask2features_ellipse3(self):
-        # print "---Testing binary_mask2ellipse_features for
+    def test_mask2features_poly_ellipse3(self):
+        # print "---Testing binary_mask2ellipse_features_single for
         # ellipse3----------------"
-        num_regions, features = sr.helpers.binary_mask2ellipse_features_single(
+        _, _, features_poly = sr.helpers.binary_mask2ellipse_features_single(
             self.ellipse3_mask, self.connectivty, 2)
 
 #        print "MATLAB features:", self.features_ellipse3
@@ -126,16 +118,12 @@ class HelpersEllipseTester(unittest.TestCase):
 #        print 'Max abs. difference: ',  np.max(np.max( np.abs(features - self.features_ellipse3)))
 # print
 # '********************************************************************************************'
-        assert sr.helpers.array_diff(
-            self.features_ellipse3,
-            features,
-            self.rtol,
-            self.atol)
+        assert sr.helpers.array_diff( self.features_poly_ellipse3, features_poly,self.rtol, self.atol)
 
-    def test_mask2features_ellipse4(self):
-        # print "---Testing binary_mask2ellipse_features for
+    def test_mask2features_poly_ellipse4(self):
+        # print "---Testing binary_mask2ellipse_features_single for
         # ellipse4----------------"
-        num_regions, features = sr.helpers.binary_mask2ellipse_features_single(
+        _, _, features = sr.helpers.binary_mask2ellipse_features_single(
             self.ellipse4_mask, self.connectivty, 2)
 
 #        print "MATLAB features:", self.features_ellipse4
@@ -144,8 +132,4 @@ class HelpersEllipseTester(unittest.TestCase):
 #        print 'Max abs. difference: ',  np.max(np.max( np.abs(features - self.features_ellipse4)))
 # print
 # '********************************************************************************************'
-        assert sr.helpers.array_diff(
-            self.features_ellipse4,
-            features,
-            self.rtol,
-            self.atol)
+        assert sr.helpers.array_diff(self.features_poly_ellipse4,  features, self.rtol, self.atol)
