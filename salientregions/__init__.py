b76afb89740cbf1ecafa259d4779896bc3697583
elboyran <e.ranguelova@esciencecenter.nl>
2016-07-14 14:39:48 +0200

    first module docstring

--- salientregions/__init__.py
+++ salientregions/__init__.py
@@ -1,3 +1,6 @@
+'''
+The initialization module for the salient regions package.
+'''
 from __future__ import absolute_import
 from .helpers import show_image, read_matfile, \
     image_diff, visualize_elements, binary_mask2ellipse_features, visualize_ellipses,\
