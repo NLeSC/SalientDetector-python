708933f10737184c2e3b4158d345272aa2a26dcf
dafnevk <d.vankuppevelt@esciencecenter.nl>
2016-07-15 11:14:23 +0200

    Slightly different configuration for pypi

--- setup.py
+++ setup.py
@@ -4,15 +4,13 @@ Setting up the Salient region detection in images package.
 '''
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme = f.read()
-
 setup(
     name='salientregions',
-    version='0.0.1',
+    version='1.0.0',
     description='Package for finding salient regions in images',
-    long_description=readme,
+    #long_description=readme,
     author='Netherlands eScience Center',
     url='https://github.com/NLeSC/SalientRegions-python',
+    download_url = 'https://github.com/NLeSC/SalientDetector-python/tarball/v1.0.0',
     packages=find_packages(exclude=('tests'))
 )
