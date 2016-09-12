f32a6f413ac1771b4d685c34609e140758a2ac28
Carlos Martinez <c.martinez@esciencecenter.nl>
2016-09-12 15:45:44 +0200

    Update to use opencv wheel

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@
 Setting up the Salient region detection in images package.
 '''
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
 with open('README.md') as f:
     readme = f.read()
 
+# Parse requirements.txt file
+lines = parse_requirements('requirements.txt', session=False)
+requirements = [str(item.req) for item in lines]
+
 setup(
     name='salientregions',
     version='0.0.1',
+    install_requires=requirements,
     description='Package for finding salient regions in images',
     long_description=readme,
     author='Netherlands eScience Center',
